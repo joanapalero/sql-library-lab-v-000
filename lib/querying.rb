a7e60e92217d1a3e1f9b51d7e712bbb493c599f2
Joana Palero <joana.palero24@gmail.com>
2018-07-29 15:34:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,12 +19,16 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name 
   FROM authors 
-  INNER JOIN series ON authors.id = series.author_id 
-  JOIN subgenres ON series.subgenre_id = subgenres.id;"
+    INNER JOIN series ON authors.id = series.author_id 
+    JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human' GROUP BY(series.title) ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+  "SELECT series.title 
+  FROM series 
+    INNER JOIN characters ON series.id = characters.series_id 
+    WHERE characters.species = 'human' GROUP BY(series.title) 
+    ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
